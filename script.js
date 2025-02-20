677446996b2f6695d6e9124bf79347ea395cf851
Vinson <vinsonhe2@gmail.com>
2025-02-20 14:52:48 -0800

    Add player factory and gameController module
    Added ability to place tokens and switch turns
    Also renamed some function to be more clear and made code cleaner

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
-const gameBoard = function() {
+const cell = function() {
+    let cellValue = '';
+
+    const getValue = () => cellValue;
+
+    const setValue = (value) => {
+        cellValue = value;
+    }
+
+    const checkTaken = () => {
+        return (cellValue !== '') ? true : false;
+    }
+
+    return { setValue, getValue, checkTaken };
+}
+
+const gameBoard = (function() {
     const rows = 3;
     const columns = 3;
     const board = [];
@@ -6,40 +22,70 @@ const gameBoard = function() {
     for (let i = 0; i < rows; i++) {
         board.push([]);
         for (let n = 0; n < columns; n++) {
-            // n will become a cell function later
             board[i].push(cell());
         }
     } 
 
     const getBoard = () => board;
 
-    const printBoard = () => {
+    const logBoard = () => {
         let boardString = '';
+
         board.map((row) => {
             row.map((cell) => {
-                boardString += `${cell.getValue()}  `;
-                console.log(cell.getValue());
+                boardString += `|${cell.getValue()}|  `;
             });
             boardString += '\n\n';
         });
+
         console.log(boardString);
     }
 
-    printBoard();
-
-    return { getBoard, printBoard };
-};
-
-const cell = function() {
-    let cellValue = 'X';
-
-    const placeToken = (value) => {
-        cellValue = value;
+    const setToken = (row, column, token) => {
+        board[row][column].setValue(token);
     }
 
-    const getValue = () => cellValue;
+    const checkCell = (row, column, token) => {
+        if (board[row][column].checkTaken(token)) return true;
+    }
 
-    return { placeToken, getValue }
+    return { getBoard, logBoard, setToken, checkCell };
+})();
+
+const createPlayer = function(name, token) {
+    const getName = () => name;
+    const getToken = () => token; 
+
+    return { getName, getToken };
 }
 
-const newBoard = gameBoard();
+const gameController = (function() {
+    const playerOne = createPlayer('Player 1', 'X');
+    const playerTwo = createPlayer('Player 2', 'O');
+    let currentTurn = playerOne;
+    let currentToken = currentTurn.getToken();
+
+    const switchTurn = () => {
+        if (currentTurn.getName() === playerOne.getName()) {
+            currentTurn = playerTwo;
+            currentToken = currentTurn.getToken();
+        } else {
+            currentTurn = playerOne;
+            currentToken = currentTurn.getToken();
+        }
+    }
+
+    const placeToken = (row, column) => {
+        if (gameBoard.checkCell(row, column, currentToken)) {
+            return console.log('Cell is already taken!');
+        }
+        gameBoard.setToken(row, column, currentToken);
+        switchTurn();
+        gameBoard.logBoard();
+    }
+
+    console.log('Let\'s play Tic, Tac, and Toe!')
+    console.log(gameBoard.logBoard());
+
+    return { placeToken, switchTurn };
+})();
